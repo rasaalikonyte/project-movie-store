5d450e41d8766066bf7543081ee4af4dd263a390
Your Name <rasa.alikonyte@gmail.com>
2024-05-27 11:26:42 +0300

    post

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 
+let movies = [];
+
+// Create a new movie
+app.post("/movies", (req, res) => {
+  const { title, productionDate, producer, rating } = req.body;
+  const movie = {
+    id: movies.length + 1,
+    title,
+    productionDate,
+    producer,
+    rating,
+  };
+  movies.push(movie);
+  res.status(201).send(movie);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
