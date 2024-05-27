7ea410426674b8a9255218955213a4f734246ad9
Your Name <rasa.alikonyte@gmail.com>
2024-05-27 11:25:30 +0300

    server connection

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
-
+// const port = 3000;
+const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
